0c03c140d18bb63a3ba2a07ffc120cf7c4c28135
vanshsharma69 <vanshsharma1750@gmail.com>
2026-01-30 12:17:19 +0530

    project ready

---
+++ src/components/NavBar.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const NavBar = () => {
+  return (
+    <div>
+      this is a navbar component
+    </div>
+  )
+}
+
+export default NavBar
---
+++ src/context/AppContext.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const AppContext = () => {
+  return (
+    <div>
+      this is app context component
+    </div>
+  )
+}
+
+export default AppContext
